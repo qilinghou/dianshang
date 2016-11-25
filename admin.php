9eb19cf9ea6ecf8eeaba38a80657d64671a9ee5a
gugu <cxiangk@163.com>
2016-11-25 14:13:46 +0800

    test file add echo

--- admin.php
+++ admin.php
@@ -18,11 +18,11 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',true);
 define('BIND_MODULE', 'Admin');
 
-
+//test1 admin.php
 // 定义应用目录
 define('APP_PATH','./App/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
-
-// 亲^_^ 后面不需要任何代码了 就是如此简单
+//2test admin.php
+//亲^_^ 后面不需要任何代码了 就是如此简单

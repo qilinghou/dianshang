7a8623e651d3d38f36c19395d843402fa52a2f31
gugu <cxiangk@163.com>
2017-02-15 12:18:44 +0800

    update index.php

--- admin.php
+++ admin.php
@@ -18,11 +18,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',true);
 define('BIND_MODULE', 'Admin');
 
-//test1 admin.php
 // 定义应用目录
 define('APP_PATH','./App/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
-//2test admin.php
 //亲^_^ 后面不需要任何代码了 就是如此简单
